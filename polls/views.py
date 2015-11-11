aca749aca9aef6f2c9929342f075f991da346ea4
Yoluchi <yantaq@att.net>
2015-11-10 16:34:16 -0800

    finished template/view

--- polls/views.py
+++ polls/views.py
@@ -1,27 +1,19 @@
-from django.http import HttpResponse
-from django.template import RequestContext, loader
-
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-	    'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    template = loader.get_template('polls/returnHome.html')
-    context = RequestContext(request, {
-	    'question_id': question_id,
-    })
-    return HttpResponse(template.render(context))
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of quesiton %s."
-    return HttpResponse(response % question_id)
+    return render(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on quesiton %s." % question_id)
+    return render(request, "You're voting on quesiton %s." % question_id)
