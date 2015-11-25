486eb60277b590938b08c331416afdfbba79a19a
Yoluchi <yantaq@att.net>
2015-11-25 00:54:58 -0800

    updated todaylearned app

--- todaylearned/urls.py
+++ todaylearned/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     # ex: /entry/
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<entry_id>[0-9]+)/$', views.details, name='details'),
 ]
--- todaylearned/views.py
+++ todaylearned/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from . models import Entry
 
 # Create your views here.
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 def index(request):
    entry = get_list_or_404(Entry.objects.order_by('-modified'))
    context = {'entry': entry}   
    return render(request, 'todaylearned/index.html', context)
+
+def details(request, entry_id):
+   entry = get_object_or_404(Entry, pk=entry_id)
+   context = {'entry': entry}   
+   return render(request, 'todaylearned/details.html', context)
