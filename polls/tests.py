f3ed4d8446a515c84eef438de783fbf0f6d2087e
Yoluchi <yantaq@att.net>
2015-12-01 23:38:12 -0800

    update test

--- polls/tests.py
+++ polls/tests.py
@@ -8,7 +8,6 @@ from .models import Question
 class QuestionMethodTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
-
         """
         was_published_recently() should return False for questions
         whose pub_date is in the future
@@ -16,4 +15,23 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertEqual(future_question.was_published_recently(), False)
+    
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently should return False for questions
+        whose pub_date is older than one day
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertEqual(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently should return True for questions
+        whose pub_date is less than one day old
+        """
+        time = timezone.now() - datetime.timedelta(hours=4)
+        recent_question = Question(pub_date=time)
+        self.assertEqual(recent_question.was_published_recently(), True)
+
     
