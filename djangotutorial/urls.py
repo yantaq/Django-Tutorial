d0520264d704fb8226071bd5a523ad0b88df67a3
Yoluchi <yantaq@att.net>
2015-12-10 14:48:43 -0800

    improved template structure

--- djangotutorial/urls.py
+++ djangotutorial/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('polls.urls')),
+    url(r'^$', include('todaylearned.urls')),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^entry/', include('todaylearned.urls', namespace="todaylearned")),
     url(r'^admin/', include(admin.site.urls)),
