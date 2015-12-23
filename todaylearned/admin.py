dcde85e7c53d7e66b4002c7a45bf3ab0b522249b
Yoluchi <yantaq@att.net>
2015-12-22 22:49:08 -0800

    updated admin with filter and search

--- todaylearned/admin.py
+++ todaylearned/admin.py
@@ -9,6 +9,10 @@ class EntryAdmin(admin.ModelAdmin):
     formfield_overrides = {
         models.CharField: {'widget': TextInput(attrs={'size':'50'})},
         models.TextField: {'widget': Textarea(attrs={'rows':14, 'cols':150})},    }
+
+    list_filter = ['modified']
+    search_fields = ['content']
+    
     
 
 admin.site.register(Entry, EntryAdmin)
