f8a11b521dbfeaf2e27541a16fe61a40487ccf1b
Yoluchi <yantaq@att.net>
2015-12-23 17:33:59 -0800

    added Q model

--- oqu/models.py
+++ oqu/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Quran(models.Model):
+    database_id = models.CharField(max_length=5)
+    sura_id = models.CharField(max_length=3)
+    verse_id = models.CharField(max_length=3)
+    ayat_text = models.TextField()
+    
